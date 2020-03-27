43f49237b711999298db9746f1e1728751ab5058
Behailu Lemma <behailu.lemma12@gmail.com>
2020-03-27 02:22:10 -0500

    first commit

---
+++ src/main/java/com/beh/rewardsws/RewardsWsApplication.java
@@ -0,0 +1,13 @@
+package com.beh.rewardsws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beh/rewardsws/RewardsWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beh.rewardsws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
